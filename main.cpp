d6b85ad9d8c22a439a9eddc5dc3087913bbf8338
Abhishek-Padhwal <abhishekpadhwal@gmail.com>
2024-07-08 17:08:50 +0530

    Final project

--- main.cpp
+++ main.cpp
@@ -1,96 +1,93 @@
-//C++ Program to Solve Sudoku Problem
 #include <iostream>
-using namespace std;
-#define N 9        // N is the size of the matrix i.e., N*N
-//Function to print the matrix
-void print(int arr[N][N])
-{
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-			cout << arr[i][j] << " ";
-		cout << endl;
-	}
-}
-//Checks whether it will be legal to assign number to the given row, col
-bool isValid(int puzzle[N][N], int row,	int col, int number)
-{
-	//Check if we find the same number in the similar row , we return false
-	for (int x = 0; x <= 8; x++)
-		if (puzzle[row][x] == number)
-			return false;
-	//Check if we find the same number in the similar column, we return false
-	for (int x = 0; x <= 8; x++)
-		if (puzzle[x][col] == number)
-			return false;
-	/*Check if we find the same number in
-	 the particular 3*3 matrix,
-	 we return false*/
-	int sRow = row - row % 3,
-			sCol = col - col % 3;
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 3; j++)
-			if (puzzle[i + sRow][j +
-							sCol] == number)
-				return false;
-	return true;
-}
-//Function to Solve the sudoku puzzle
-bool solution(int puzzle[N][N], int row, int col)
-{
-	/* If the 8th row and 9th column (0 indexed matrix) is reached, 
-	then return true to avoid backtracking*/
-	if (row == N - 1 && col == N)
-		return true;
-	/*If column value becomes 9, move to next row and column 
-	Now start from 0*/
-	if (col == N) {
-		row++;
-		col = 0;
-	}
-	/*If the current position contains value >0, then iterate for next column*/
-	if (puzzle[row][col] > 0)
-		return solution(puzzle, row, col + 1);
-	for (int number = 1; number <= N; number++)
-	{
-		//Check whether a number (1-9) can be placed in the given row,col
-		if (isValid(puzzle, row, col, number))
-		{	
-		/* Let the number is present in the current(row,col)
-        position of the puzzle and let the assigned  
-        number in that position is correct*/
-			puzzle[row][col] = number;
-			// Checking for next possibility with next column
-			if (solution(puzzle, row, col + 1))
-				return true;
-		}
-	/* If assumption is wrong, then remove the assigned number
-	Proceed with a different value for next assumption*/
-		puzzle[row][col] = 0;
-	}
-	return false;
-}
-// Driver Code
-int main()
-{
-	// Here 0 means unassigned cells
-	int puzzle[N][N] = { { 0, 7, 0, 0, 0, 0, 0, 0, 9 },
-					{ 5, 1, 0, 4, 2, 0, 6, 0, 0 },
-					{ 0, 8, 0, 3, 0, 0, 7, 0, 0 },
-					{ 0, 0, 8, 0, 0, 1, 3, 7, 0 },
-					{ 0, 2, 3, 0, 8, 0, 0, 4, 0 },
-					{ 4, 0, 0, 9, 0, 0, 1, 0, 0 },
-					{ 9, 6, 2, 8, 0, 0, 0, 3, 0 },
-					{ 0, 0, 0, 0, 1, 0, 4, 0, 0 },
-					{ 7, 0, 0, 2, 0, 3, 0, 9, 6 } };
-        cout << "Before Solving " << endl;
-    	print(puzzle);
-    	cout << "" << endl;
-    	cout << "After Solving " << endl;
-	if (solution(puzzle, 0, 0))
-		print(puzzle);
-	else
-		cout << "No solution exists " << endl;
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <iomanip>
 
-	return 0;
+using namespace std;
+
+const int SIZE = 9;
+const int EMPTY = 0;
+
+void printSudoku(const vector<vector<int>>& board) {
+    system("cls"); // For Linux/Unix systems, use "cls" for Windows
+
+    cout << "+-------+-------+-------+" << endl;
+    for (int row = 0; row < SIZE; ++row) {
+        cout << "| ";
+        for (int col = 0; col < SIZE; ++col) {
+            if (board[row][col] == EMPTY) {
+                cout << ". ";
+            } else {
+                cout << board[row][col] << " ";
+            }
+            if ((col + 1) % 3 == 0) cout << "| ";
+        }
+        cout << endl;
+        if ((row + 1) % 3 == 0) cout << "+-------+-------+-------+" << endl;
+    }
+    this_thread::sleep_for(chrono::milliseconds(100));
+}
+
+bool isValid(const vector<vector<int>>& board, int row, int col, int num) {
+    for (int x = 0; x < SIZE; ++x) {
+        if (board[row][x] == num || board[x][col] == num) {
+            return false;
+        }
+    }
+    int startRow = row - row % 3;
+    int startCol = col - col % 3;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (board[i + startRow][j + startCol] == num) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool solveSudoku(vector<vector<int>>& board) {
+    for (int row = 0; row < SIZE; ++row) {
+        for (int col = 0; col < SIZE; ++col) {
+            if (board[row][col] == EMPTY) {
+                for (int num = 1; num <= SIZE; ++num) {
+                    if (isValid(board, row, col, num)) {
+                        board[row][col] = num;
+                        printSudoku(board);
+                        if (solveSudoku(board)) {
+                            return true;
+                        }
+                        board[row][col] = EMPTY;
+                        printSudoku(board);
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    vector<vector<int>> board = {
+    {5, 3, 0, 6, 7, 0, 9, 0, 2},
+    {0, 0, 2, 1, 0, 0, 3, 0, 8},
+    {1, 9, 0, 3, 4, 2, 5, 6, 7},
+    {0, 5, 0, 0, 0, 1, 4, 2, 3},
+    {4, 2, 0, 8, 0, 3, 7, 0, 1},
+    {7, 1, 0, 0, 2, 0, 8, 5, 6},
+    {9, 0, 1, 5, 0, 7, 2, 8, 4},
+    {2, 8, 0, 0, 1, 0, 0, 0, 5},
+    {3, 0, 5, 2, 0, 6, 1, 7, 9}
+};
+
+
+    if (solveSudoku(board)) {
+        cout << "Sudoku solved successfully!" << endl;
+    } else {
+        cout << "No solution exists for the given Sudoku." << endl;
+    }
+
+    return 0;
 }
